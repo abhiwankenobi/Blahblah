9f29a53d6cf7eaf5017979ffef22e620a29d86fe
Abhishek Iyer <36741282+abhiwankenobi@users.noreply.github.com>
2023-07-06 10:18:43 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,256 @@
+from masced_bandits.bandits import init_bandit
+from masced_bandits.bandit_options import initialize_arguments
+from environmentgrammar import environment_grammar, EnvironmentTransformer
+from lark import Lark
+import numpy as np
+import matplotlib.pyplot as plt
+from statistics import mean
+import pprint 
+from math import floor
+from managedsystem import MockSAS
+import os
+import userfunctions
+import sys
+import csv
+LINE_DATA_POINTS = 10
+TOTAL_ROUNDS = 0
+NUM_SEEDS = 30
+CSV_name = "resultDingNet2.csv" #CHANGE FILE NAME HERE
+
+SWIM_ORDERING = {"UCB-TN" : 1,
+"egreedy-0.2" : 2,
+"DUCB-0.997" : 3,
+"DUCB-0.995" : 4,
+"egreedy-0.4" : 5,
+"EXP3-333" : 6,
+"DUCB-0.992" : 7,
+"DUCB-0.99" : 8,
+"egreedy-0.6" :9 ,
+"egreedy-0.8" : 10,
+"DUCB-0.97" : 11,
+"DUCB-0.95" : 12,
+"egreedy-1.0" : 13,
+"DUCB-0.92" : 14,
+"DUCB-0.89" : 15}
+plt.style.use('seaborn-bright')
+parser = Lark(environment_grammar)
+mission_statement = "profiles/DingNetProfile.txt" #CHANGE PROFILES HERE!
+
+sys_name = mission_statement.split(".txt")[0]
+
+result_path = "results/" + sys_name + "/" 
+os.makedirs(result_path, exist_ok=True)
+
+if len(sys.argv) == 3:
+    NUM_SEEDS = int(sys.argv[1]) 
+    CSV_name = str(sys.argv[2])
+
+
+try:
+    with open(mission_statement, 'r') as source:
+        source_code = source.read()
+
+except IOError:
+    print('Something is wrong with the source code file')
+
+try:
+    parse_tree = parser.parse(source_code)
+    #print(parse_tree.pretty())
+    
+    
+except Exception as e:
+    print("Syntax error, details below: \n")
+    print(e)
+    exit(1)
+
+final_res = {}
+
+def describe_config(config):
+    return str(str(config['name']) + "-" +  str(config.get('formula',"")) + str(config.get('epsilon',"")) + str(config.get('exploration_rounds',"")) + str(config.get('decay_rate',"")) + str(config.get('learning_rate',"")) + str(config.get('horizon',"")) + str(config.get('gamma',"")) )
+
+
+# egreedies = [[{'name': "egreedy", 'epsilon': "0.1"}], [{'name': "egreedy", 'epsilon': "0.2"}],[{'name': "egreedy", 'epsilon': "0.3"}], \
+# [{'name': "egreedy", 'epsilon': "0.4"}],[{'name': "egreedy", 'epsilon': "0.5"}], [{'name': "egreedy", 'epsilon': "0.6"}],[{'name': "egreedy", 'epsilon': "0.7"}], \
+# [{'name': "egreedy", 'epsilon': "0.8"}],[{'name': "egreedy", 'epsilon': "0.9"}],[{'name': "egreedy", 'epsilon': "1.0"}]]
+
+egreedies = [{'name': "egreedy", 'epsilon': "0.2"}, {'name': "egreedy", 'epsilon': "0.4"},{'name': "egreedy", 'epsilon': "0.6"}, \
+{'name': "egreedy", 'epsilon': "0.8"},{'name': "egreedy", 'epsilon': "1.0"}]
+
+ucbs = [{'name': "UCB", 'formula': "TN"}]
+
+
+# exp3s = [[{'name': "EXP3", 'learning_rate': "0.1"}], \
+# [{'name': "EXP3", 'learning_rate': "0.2"}],[{'name': "EXP3", 'learning_rate': "0.3"}], [{'name': "EXP3", 'learning_rate': "0.4"}], [{'name': "EXP3", 'learning_rate': "0.5"}],\
+# [{'name': "EXP3", 'learning_rate': "0.6"}], [{'name': "EXP3", 'learning_rate': "0.7"}], [{'name': "EXP3", 'learning_rate': "0.8"}], [{'name': "EXP3", 'learning_rate': "0.9"}], \
+# [{'name': "EXP3", 'learning_rate': "1.0"}]]   
+
+exp3s =  [{'name': "EXP3", 'horizon': "333"}]
+
+discountucbs = [{'name': "DUCB", 'gamma': "0.89"}, {'name': "DUCB", 'gamma': "0.92"}, {'name': "DUCB", 'gamma': "0.95"}, {'name': "DUCB", 'gamma': "0.97"}, {'name': "DUCB", 'gamma': "0.99"},\
+    {'name': "DUCB", 'gamma': "0.992"}, {'name': "DUCB", 'gamma': "0.995"}, {'name': "DUCB", 'gamma': "0.997"}]
+
+configs = egreedies + ucbs + exp3s + discountucbs
+
+#simple_configs = [ [{'name': "ETC", 'exploration_rounds': "1"}], [{'name': "UCB", 'formula': "TN"}], [{'name': "egreedy", 'epsilon': "0.1"}], [{'name': "egreedy", 'epsilon': "0.2"}],[{'name': "egreedy", 'epsilon': "0.3"}]]
+
+#configs = [[{'name': "ETC", 'exploration_rounds': "1"}], [{'name': "UCB", 'formula': "TN"}], [{'name': "egreedy", 'epsilon': "0.1"}], [{'name': "EXP3", 'learning_rate': "0.03"}], [{'name': "EXP3", 'learning_rate': "0.05"}], [{'name': "EXP3", 'learning_rate': "0.07"}], [{'name': "EXP3", 'learning_rate': "0.08"}]]
+
+for config in configs:
+    final_res[describe_config(config)] = {"boxplot": [], "lineplot" : [], "positions": [], "match_dingnet": []}
+
+for i in range(NUM_SEEDS):
+    all_res = {}
+    for config in configs:
+        #input(str(config) + ">")
+        np.random.seed(i * 293019)
+        mksas = MockSAS(config, parse_tree)
+        res = mksas.operation({})
+        for key in res.keys():
+            #input(str(key) + ">>")
+            #input(str(res[key][-1]) + ">>>")
+            config_key = describe_config(config)
+            final_res[config_key]['boxplot'].append((res[key][-1]))
+            every_nth = floor(len(res[key]) / LINE_DATA_POINTS)
+            TOTAL_ROUNDS = len(res[key])
+            line_points = res[key][::every_nth]
+            final_res[config_key]['lineplot'].append(line_points)
+        
+    position_dict = {}
+    for config in configs:
+        for key in res.keys():
+            config_key = describe_config(config)
+            position_dict[config_key] = final_res[config_key]['boxplot'][-1]
+
+    ranking = (list({k: v for k, v in sorted(position_dict.items(), key=lambda item: item[1], reverse= True)}.keys()))
+
+    for policy in ranking:
+        final_res[policy]["match_dingnet"].append(1 if ((ranking.index(policy)+1) == SWIM_ORDERING[policy]) else 0)
+        final_res[policy]["positions"].append(ranking.index(policy)+1)
+
+
+#SWIM
+
+#
+
+# pprint.pprint(final_res)
+# print(str(final_res.keys()))
+
+# boxplot_data = []
+# boxplot_labels = []
+
+# lineplot_data = {}
+# for config_key in final_res.keys():
+#     boxplot_data.append(final_res[config_key]["boxplot"])
+#     boxplot_labels.append(str(config_key))
+
+#     stacked_data = final_res[config_key]["lineplot"]
+#     # pprint.pprint(stacked_data)
+#     # input("stacked data")
+#     # for run_tuple in zip(*stacked_data):
+#     #     pprint.pprint(run_tuple)
+#     #     input("zip_tuple")
+#     lineplot_data[str(config_key)] = [mean(run_tuple) for run_tuple in zip(*stacked_data)]
+
+# row_titles = ["Policy Name", "Mean", "Median", "Upper Q", "Lower Q", "IQR", "Upper W", "Lower W", "Average Ranking", "Percentage Matching SWIM"]
+# all_rows = [row_titles]
+# for i, datum in enumerate(boxplot_data):
+
+#     entry_row = []
+
+#     entry_row.append(boxplot_labels[i])
+
+#     data_mean = np.mean(datum)
+#     median = np.median(datum)
+#     upper_quartile = np.percentile(datum, 75)
+#     lower_quartile = np.percentile(datum, 25)
+
+#     iqr = upper_quartile - lower_quartile
+#     upper_whisker = np.array(datum)[datum<=upper_quartile+1.5*iqr].max()
+#     lower_whisker = np.array(datum)[datum>=lower_quartile-1.5*iqr].min()
+
+#     average_rank = np.mean(final_res[boxplot_labels[i]]["positions"])
+#     perct_match = str(np.mean(final_res[boxplot_labels[i]]["match_swim"]) * 100) + "%"
+
+
+#     entry_row+=[median,data_mean,upper_quartile,lower_quartile,iqr,upper_whisker,lower_whisker, average_rank, perct_match]
+
+#     all_rows.append(entry_row)
+
+
+#DingNet
+pprint.pprint(final_res)
+print(str(final_res.keys()))
+
+boxplot_data = []
+boxplot_labels = []
+
+lineplot_data = {}
+for config_key in final_res.keys():
+    boxplot_data.append(final_res[config_key]["boxplot"])
+    boxplot_labels.append(str(config_key))
+
+    stacked_data = final_res[config_key]["lineplot"]
+    # pprint.pprint(stacked_data)
+    # input("stacked data")
+    # for run_tuple in zip(*stacked_data):
+    #     pprint.pprint(run_tuple)
+    #     input("zip_tuple")
+    lineplot_data[str(config_key)] = [mean(run_tuple) for run_tuple in zip(*stacked_data)]
+
+row_titles = ["Policy Name", "Mean", "Median", "Upper Q", "Lower Q", "IQR", "Upper W", "Lower W", "Average Ranking", "Percentage Matching DingNet"]
+all_rows = [row_titles]
+for i, datum in enumerate(boxplot_data):
+
+    entry_row = []
+
+    entry_row.append(boxplot_labels[i])
+
+    data_mean = np.mean(datum)
+    median = np.median(datum)
+    upper_quartile = np.percentile(datum, 75)
+    lower_quartile = np.percentile(datum, 25)
+
+    iqr = upper_quartile - lower_quartile
+    upper_whisker = np.array(datum)[datum<=upper_quartile+1.5*iqr].max()
+    lower_whisker = np.array(datum)[datum>=lower_quartile-1.5*iqr].min()
+
+    average_rank = np.mean(final_res[boxplot_labels[i]]["positions"])
+    perct_match = str(np.mean(final_res[boxplot_labels[i]]["match_dingnet"]) * 100) + "%"
+
+
+    entry_row+=[median,data_mean,upper_quartile,lower_quartile,iqr,upper_whisker,lower_whisker, average_rank, perct_match]
+
+    all_rows.append(entry_row)
+
+
+with open(result_path + CSV_name, 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile)
+    for the_row in all_rows:
+        spamwriter.writerow(the_row)
+
+
+
+
+def boxplotter(boxplot_data, boxplot_labels, result_path):
+    plt.boxplot(boxplot_data, showmeans=True, meanline=True)
+    plt.ylabel("average reward after " + str(TOTAL_ROUNDS-1) + " rounds (" + str(NUM_SEEDS) + " seeds)", fontdict={'size':14})
+    plt.xlabel("policies", fontdict={'size':14})
+
+    plt.xticks(ticks=list(range(1,len(configs)+1,1)),labels=boxplot_labels)
+    plt.tight_layout()
+
+    plt.savefig(result_path + "boxplot.pdf") 
+    plt.cla()
+
+def lineplotter(lineplot_data,result_path):
+    for line_key in lineplot_data.keys():
+        plt.plot(lineplot_data[line_key], label=str(line_key))
+
+    plt.xticks(ticks= list(range(0,LINE_DATA_POINTS+1,1)), labels = [num for num in range(0,TOTAL_ROUNDS+1,floor(TOTAL_ROUNDS / LINE_DATA_POINTS))])
+    plt.legend(fontsize='medium', title='policy')
+    plt.ylabel("average reward", fontdict={'size':14})
+    plt.xlabel("rounds elapsed", fontdict={'size':14})
+    plt.tight_layout()
+
+    plt.savefig(result_path + "lineplot.pdf") 
+    plt.cla()
---
+++ userfunctions.py
@@ -0,0 +1,341 @@
+from scipy.stats import truncnorm
+import gym
+from gym import spaces
+import numpy as np
+import ast
+import os
+import pandas as pd
+import xml.etree.ElementTree as ET
+from glob import glob
+
+
+def myfunction(posarg, posarg2):
+    print(posarg)
+    return (posarg + posarg2) / 10
+
+def truncated_normal(lower, upper, mean, stdev):
+    a, b = (lower - mean) / stdev, (upper - mean) / stdev
+    return truncnorm.rvs(a, b, loc=mean, scale=stdev)
+
+def normal(mean, stdev):
+    return np.random.normal(loc=mean, scale=stdev)
+
+def uniform(lower, upper):
+    return np.random.uniform(low=lower, high=upper)
+
+def truncate(utility):
+    bounds = (140, 300)
+    lower_bound, upper_bound = bounds
+
+    if utility > upper_bound:
+        utility = upper_bound
+    elif utility < lower_bound:
+        utility = lower_bound
+
+    range_ = upper_bound - lower_bound
+    result = float((utility - lower_bound) / range_)
+    return result
+
+def utilitySWIM(arrival_rate, dimmer, avg_response_time, max_servers, servers):
+    OPT_REVENUE = 1.5
+    BASIC_REVENUE = 1
+    SERVER_COST = 10
+    RT_THRESH = 0.75
+
+    ur = arrival_rate * ((1 - dimmer) * BASIC_REVENUE + dimmer * OPT_REVENUE)
+    uc = SERVER_COST * (max_servers - servers)
+    urt = 1 - ((avg_response_time - RT_THRESH) / RT_THRESH)
+
+    UPPER_RT_THRESHOLD = RT_THRESH * 4
+    delta_threshold = UPPER_RT_THRESHOLD - RT_THRESH
+    UrtPosFct = (delta_threshold / RT_THRESH)
+
+    urt_final = None
+    if avg_response_time <= UPPER_RT_THRESHOLD:
+        urt = ((RT_THRESH - avg_response_time) / RT_THRESH)
+    else:
+        urt = ((RT_THRESH - UPPER_RT_THRESHOLD) / RT_THRESH)
+
+    if avg_response_time <= RT_THRESH:
+        urt_final = urt * UrtPosFct
+    else:
+        urt_final = urt
+
+    revenue_weight = 0.7
+    server_weight = 0.3
+    utility = urt_final * ((revenue_weight * ur) + (server_weight * uc))
+
+    truncated_reward = truncate(utility)
+    print(truncated_reward)
+    return truncated_reward
+
+#############################################################################################################
+#EVERYTHING BELOW HERE BASIC DATA PROCESSING.
+
+def xml_to_df(root, path):
+    data = []
+    for element in root.findall(path):
+        row = {}
+        for child in element:
+            row[child.tag] = child.text
+        for key, value in element.attrib.items():
+            row[key] = value
+        data.append(row)
+    df = pd.DataFrame(data)
+    return df
+
+# Define the base directory
+base_dir = "Final Runs" #Change this to your base directory! 
+
+# Define the environments
+environments = ["City", "Forest", "Plains"]
+
+# Define the power settings
+power_settings = [f"PS {i}" for i in range(1, 15)]
+
+# Define the runs
+runs = [f"Run {i}" for i in range(1, 4)]
+
+# The DataFrame to hold all the data
+all_data = pd.DataFrame()
+
+# Iterate over environments, power settings, and runs
+for environment in environments:
+    for power_setting in power_settings:
+        for run in runs:
+            run_dir = os.path.join(base_dir, environment, power_setting, run)
+            
+            # Parse the main XML file
+            main_file = os.path.join(run_dir, "main.xml")
+            tree = ET.parse(main_file)
+            root = tree.getroot()
+            df_main = xml_to_df(root, './/mote[number="1"]/receivedTransmissions/receivedTransmission')
+            
+            # Parse the mote XML files
+            mote_files = glob(os.path.join(run_dir, "mote_*.xml"))
+            df_motes = pd.concat([xml_to_df(ET.parse(file).getroot(), './/transmissionPower') for file in mote_files])
+            
+            # Ensure 'moteIdentifier' and 'transmissionPower' are the same type (string)
+            df_main[['moteIdentifier', 'transmissionPower']] = df_main[['moteIdentifier', 'transmissionPower']].astype(str)
+            df_motes[['moteIdentifier', 'transmissionPower']] = df_motes[['moteIdentifier', 'transmissionPower']].astype(str)
+            
+            # Merge the data based on 'moteIdentifier' and 'transmissionPower'
+            run_data = pd.merge(df_main, df_motes, on=['moteIdentifier', 'transmissionPower'])
+
+            # Add information about the run directory
+            run_data['run_directory'] = run_dir
+            
+            # Add the run data to the overall data
+            all_data = pd.concat([all_data, run_data])
+
+# Convert the string representation of tuples into actual tuples
+all_data['powerSetting'] = all_data['powerSetting'].apply(ast.literal_eval)
+
+# Split the powerSetting column into two new columns
+all_data[['transmissionCount', 'powerSetting']] = pd.DataFrame(all_data['powerSetting'].tolist(), index=all_data.index)
+
+# Ensure 'powerSetting' is integer type
+all_data['powerSetting'] = all_data['powerSetting'].astype(int)
+
+all_data['collision'] = all_data['collision'].map({'true': True, 'false': False})
+
+all_data['received'] = all_data['received'].map({'true': True, 'false': False})
+
+# Convert the 'energy' column to numeric, forcing non-numeric values to NaN
+all_data['energy'] = pd.to_numeric(all_data['energy'], errors='coerce')
+
+
+#############################################################################################################
+#EVERYTHING BELOW HERE IS FOR DINGNET. THIS IS WHERE WE WILL BE WORKING! <3
+
+
+def powerConsumed(powerSetting):
+    # Filter the data for the given environment and power setting
+    filtered_data = all_data[(all_data['powerSetting'] == powerSetting)]
+
+    # Calculate the mean and std deviation of the energy consumption
+    mean_power = filtered_data['energy'].mean()
+    std_power = filtered_data['energy'].std()
+
+    # Get the total number of trials (energy consumptions) for the given power setting and environment
+    n = len(filtered_data)
+    # print('Number of energy readings:', n)
+    
+    # number of repetitions
+    num_repetitions = 1000
+
+    # Generate multiple normal samples
+    normal_samples = np.random.normal(mean_power, std_power, num_repetitions)
+
+    return normal_samples.mean()
+
+
+
+def _packet_success(environment, powerSetting):
+    # Filter the data for the given environment and power setting
+    filtered_data = all_data[(all_data['environment'] == environment) & (all_data['powerSetting'] == powerSetting)]
+    
+    # Calculate the packet loss rate
+    _packet_success = len(filtered_data[filtered_data['received'] == True]) / len(filtered_data)
+    
+    # Get the total number of trials (packet transmissions) for the given power setting and environment
+    n = len(filtered_data)
+    # print("Legnth of data:", n)
+    
+    # number of repetitions
+    num_repetitions = 1000
+
+    # Generate multiple binomial samples
+    binomial_samples = np.random.binomial(n, (1-_packet_success), num_repetitions)
+    # print(f"Environment: {environment}, PowerSetting: {powerSetting}")
+
+    return binomial_samples.mean()
+
+
+# #Print Packet Success Rate
+# for i in range(1, 15):
+#    print("City",i,":",_packet_success("City", i))
+   
+# for i in range(1, 15):
+#    print("Forest",i,":",_packet_success("Forest", i))
+
+# for i in range(1, 15):
+#    print("Plain",i,":",_packet_success("Plain", i))
+
+# #Print Power Consumed
+# for i in range(1, 15):
+#    print("Power",i,":",powerConsumed(i))
+
+# def normalize(value, min_value, max_value):
+#     normalized_value = (value - min_value) / (max_value - min_value)
+#     return normalized_value
+
+#NEEDED TO MAKE UTILITY FUNCTION WORK! <3
+def _packet_success_forest(powerSetting):
+    return _packet_success("Forest", powerSetting)
+
+def _packet_success_city(powerSetting):
+    return _packet_success("City", powerSetting)
+
+def _packet_success_plain(powerSetting):
+    return _packet_success("Plain", powerSetting)
+
+#THIS IS MY UTILITY FUNCTION FOR DINGNET
+
+#MAYBE ADD SOME PRETTY GRAPHS TOO? >:) <3
+def _utilityDingNet(_packet_success_func, power_func, powerSetting, power_penalty_factor):
+    # compute the packet success rate using the passed function
+    _packet_success = _packet_success_func(powerSetting)
+    # compute the power consumed using the passed function
+    power_consumed = power_func(powerSetting)
+    # utility computation logic
+    utility = _packet_success - power_penalty_factor * power_consumed
+    return utility
+
+def utilityDingNet_forest(powerSetting):
+    return _utilityDingNet(_packet_success_forest, powerConsumed, powerSetting, 0.5)
+
+def utilityDingNet_city(powerSetting):
+    return _utilityDingNet(_packet_success_city, powerConsumed, powerSetting, 0.5)
+
+def utilityDingNet_plain(powerSetting):
+    return _utilityDingNet(_packet_success_plain, powerConsumed, powerSetting, 0.5)
+
+print(utilityDingNet_forest(1))
+print(utilityDingNet_forest(14))
+
+
+#EXHAUSTIVE SEARCH ALGORITHM BELOW 
+def best_power_environment():
+    best_power_settings = {}
+    for powerSetting in range(1, 15):
+        forest_utility = utilityDingNet_forest(powerSetting)
+        city_utility = utilityDingNet_city(powerSetting)
+        plain_utility = utilityDingNet_plain(powerSetting)
+        
+        if 'Forest' not in best_power_settings or forest_utility > best_power_settings['Forest'][1]:
+            best_power_settings['Forest'] = (powerSetting, forest_utility)
+
+        if 'City' not in best_power_settings or city_utility > best_power_settings['City'][1]:
+            best_power_settings['City'] = (powerSetting, city_utility)
+
+        if 'Plain' not in best_power_settings or plain_utility > best_power_settings['Plain'][1]:
+            best_power_settings['Plain'] = (powerSetting, plain_utility)
+            
+    return best_power_settings
+
+print(best_power_environment())
+
+
+#BELOW IS THE OLD EXHAUSTIVE SEARCH ALGORITHMS. THE ONE ABOVE IS THE ONE WE ARE USING.
+#############################################################################################################
+
+
+# def exhaustive_search(n_trials):
+#     # Define the environments and power settings
+#     environments = ["Forest", "City", "Plain"]
+#     power_settings = list(range(1, 15))
+    
+#     # Initialize a dictionary to store the total best power setting for each environment
+#     total_best_power_settings = {environment: 0 for environment in environments}
+    
+#     for _ in range(n_trials):
+#         for environment in environments:
+#             best_utility = -float('inf')
+#             best_power_setting = None
+            
+#             for power_setting in power_settings:
+#                 if environment == "Forest":
+#                     utility = utilityDingNet_forest(power_setting)
+#                 elif environment == "City":
+#                     utility = utilityDingNet_city(power_setting)
+#                 elif environment == "Plain":
+#                     utility = utilityDingNet_plain(power_setting)
+                
+#                 if utility > best_utility:
+#                     best_utility = utility
+#                     best_power_setting = power_setting
+            
+#             total_best_power_settings[environment] += best_power_setting
+    
+#     # Compute the average best power setting for each environment
+#     average_best_power_settings = {environment: total / n_trials for environment, total in total_best_power_settings.items()}
+    
+#     return average_best_power_settings
+
+# n_trials = 3  # Replace with the desired number of trials
+# average_best_power_settings = exhaustive_search(n_trials)
+# for environment, avg_best_power_setting in average_best_power_settings.items():
+#     print(f"The average best power setting for {environment} over {n_trials} trials is {avg_best_power_setting}")
+
+
+# def exhaustive_search():
+#     # Define the environments and power settings
+#     environments = ["Forest", "City", "Plain"]
+#     power_settings = list(range(1, 15))
+    
+#     best_configs = {}
+    
+#     for environment in environments:
+#         best_utility = -float('inf')
+#         best_power_setting = None
+        
+#         for power_setting in power_settings:
+#             if environment == "Forest":
+#                 utility = utilityDingNet_forest(power_setting)
+#             elif environment == "City":
+#                 utility = utilityDingNet_city(power_setting)
+#             elif environment == "Plain":
+#                 utility = utilityDingNet_plain(power_setting)
+            
+#             if utility > best_utility:
+#                 best_utility = utility
+#                 best_power_setting = power_setting
+        
+#         best_configs[environment] = (best_power_setting, best_utility)
+    
+#     return best_configs
+
+# best_configs = exhaustive_search()
+# for environment, (best_power_setting, best_utility) in best_configs.items():
+#     print(f"The best power setting for {environment} is {best_power_setting} with a utility of {best_utility}")
